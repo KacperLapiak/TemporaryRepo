c77a95df1c96febcfe672f3a9f8b33091362a434
KacperLapiak <68159699+KacperLapiak@users.noreply.github.com>
2020-09-24 09:35:00 +0200

    Add files via upload

---
+++ SortVectorOfStructures.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Entry
+{
+	int key_ = 0;
+	string str_ = "";
+
+	bool operator < (Entry x)
+	{
+		return this->key_ < x.key_;
+	}
+};
+
+int main()
+{
+	Entry e;
+	vector<Entry> v;
+
+	for (int i = 9; i >= 0; i--)
+	{
+		e.key_ = i;
+		e.str_ = "dane " + to_string(i);
+		v.push_back(e);
+	}
+
+	for (Entry x : v)
+		cout << x.key_ << " | " << x.str_ << endl;
+
+	sort(v.begin(), v.end());
+	cout << endl;
+
+	for (auto x : v)
+		cout << x.key_ << " | " << x.str_ << endl;
+
+	cin.get();
+}
