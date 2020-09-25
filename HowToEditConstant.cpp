f01f549e7b1ff7d833b3ba7270324afcbf66c3ae
KacperLapiak <68159699+KacperLapiak@users.noreply.github.com>
2020-09-25 18:10:15 +0200

    Add files via upload

---
+++ HowToEditConstant.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const char str[] = "Ala ma kota.";
+	int addr = (int)&str[2];
+	*(char*)addr = 'x';
+
+	cout << str;
+
+	cin.get();
+}
