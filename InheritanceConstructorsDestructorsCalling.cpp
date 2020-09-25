ec6649d48c52c8c5f07d0fe448e02f7dae323739
KacperLapiak <68159699+KacperLapiak@users.noreply.github.com>
2020-09-25 09:37:31 +0200

    Add files via upload

---
+++ InheritanceConstructorsDestructorsCalling.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class ClassA
+{
+public:
+    ClassA() { cout << "ClassA constructor\n"; }
+    ~ClassA() { cout << "ClassA destructor\n"; }
+};
+
+class ClassB : public ClassA
+{
+public:
+    ClassB() { cout << "ClassB constructor\n"; }
+    ~ClassB() { cout << "ClassB destructor\n"; }
+};
+
+class ClassC : public ClassB
+{
+public:
+    ClassC() { cout << "ClassC constructor\n"; }
+    ~ClassC() { cout << "ClassC destructor\n"; }
+};
+
+int main()
+{
+    ClassC* p = new ClassC;
+    delete p;
+
+    cin.get();
+}
