f72b93ea54b82debca8b5000ff3c2c65f447ab34
KacperLapiak <68159699+KacperLapiak@users.noreply.github.com>
2020-09-26 12:00:40 +0200

    Add files via upload

---
+++ StaticMethod.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class ExampleClass
+{
+private:
+    static int val;
+
+public:
+    static void ExampleMethod() { val++; }
+    static int ShowValue() { return val; }
+
+};
+int ExampleClass::val = 0;
+
+int main()
+{
+    ExampleClass::ExampleMethod();
+    ExampleClass::ExampleMethod();
+    cout << ExampleClass::ShowValue() << endl;
+
+    ExampleClass obj;
+    obj.ExampleMethod();
+    cout << ExampleClass::ShowValue();
+
+    cin.get();
+}
