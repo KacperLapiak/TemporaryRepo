4a557225c864b80054df92eef15007bd8e076f02
KacperLapiak <68159699+KacperLapiak@users.noreply.github.com>
2020-09-28 14:51:47 +0200

    Add files via upload

---
+++ DiamondProblem.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class ClassA
+{
+public:
+    void methodA()
+    {
+        cout << "Alice has a cat." << endl;
+    }
+};
+
+class ClassB : public virtual ClassA { };
+class ClassC : public virtual ClassA { };
+class ClassD : public ClassB, public ClassC { };
+
+int main()
+{
+    ClassD obj;
+    obj.methodA();
+
+    cin.get();
+}
