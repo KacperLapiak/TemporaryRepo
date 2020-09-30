61d237ed6471b33c25498128cf1647da7b6e227b
KacperLapiak <68159699+KacperLapiak@users.noreply.github.com>
2020-09-30 18:10:43 +0200

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void fibonacci(int x)
+{
+    int a = 0;
+    int b = 1;
+
+    for (int i = 0; i < x; i++)
+    {
+        b += a;
+        cout << b << " ";
+        a = b - a;
+    }
+}
+void fibonacciRec(int x, int a=0, int b=1)
+{
+    b += a;
+    cout << b << " ";
+    a = b - a;
+
+    x--;
+    if (x == 0) return;
+    fibonacciRec(x, a, b);
+}
+
+int main()
+{
+    fibonacci(10);
+    cout << endl;
+    fibonacciRec(10);
+
+    cin.get();
+}
